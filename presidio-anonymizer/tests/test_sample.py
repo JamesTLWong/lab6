6a2abb243dc2e93a74ae911076b7cd5a6b129b8d
James Wong <90537826+JamesTLWong@users.noreply.github.com>
2025-10-07 03:36:22 +0000

    step 3 test_sample.py

--- presidio-anonymizer/tests/test_sample.py
+++ presidio-anonymizer/tests/test_sample.py
@@ -1,6 +1,18 @@
 import pytest
 from presidio_anonymizer.sample import sample_run_anonymizer
 
-def test_sample_run_anonymizer():
-    # replace the following line with your test
-    pass
+@pytest.mark.parametrize(
+    # fmt: off
+        "text, start, end, expText, expStart, expEnd, expLen",
+    [
+        ("My name is Bond", 11, 15, "My name is BIP", 11, 14, 1
+        )    
+    ],
+    # fmt: on
+    )
+def test_sample_run_anonymizer(text, start, end, expText, expStart, expEnd, expLen):
+    result = sample_run_anonymizer(text, start, end)
+    assert result.text== expText
+    assert result.items[0].start== expStart
+    assert result.items[0].end== expEnd
+    assert len(result.items)== expLen
